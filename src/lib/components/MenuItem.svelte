b574cabde76d001238f957e9f5f7ec31d57cb93e
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-12-06 20:23:12 +0800

    add logic for interactive navigation

--- src/lib/components/MenuItem.svelte
+++ src/lib/components/MenuItem.svelte
@@ -1,22 +1,20 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { Snippet, SvelteComponent } from 'svelte';
-
 	const {
 		href,
 		// type,
-		active,
 		children
 	}: {
 		href: string;
 		// type: 'nav' | 'menu';
-		active: boolean;
 		children: Snippet;
 	} = $props();
 </script>
 
-<a {href} class={`block rounded-md px-4 py-3 ${active ? 'bg-primary-500' : ''}`}>
+<a {href} class={`block rounded-md px-4 py-3 ${$page.route.id === href ? 'bg-primary-500' : ''}`}>
 	<div
-		class={`${active ? 'fill-primary-900 font-bold text-warm-100' : ''} flex items-center gap-3 fill-primary-500 text-[1rem] text-lg`}
+		class={`${$page.route.id === href ? 'fill-primary-900 font-bold text-warm-100' : ''} flex items-center gap-3 fill-primary-500 text-[1rem] text-lg`}
 	>
 		{@render children()}
 	</div>
