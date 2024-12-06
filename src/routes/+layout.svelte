fcecf6c98b22289369215f5ce993b207ae8f1e6a
Ken Ramiscal (Laptop) <code.ramiscalken@gmail.com>
2024-12-06 13:59:36 +0800

    add afternavigate effects

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+	import { afterNavigate } from '$app/navigation';
+	import { nav } from '$lib/components/drawer.svelte';
 	import Menu from '$lib/components/Menu.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 	let { children } = $props();
+	afterNavigate(() => {
+		nav.closeMenu();
+	});
 </script>
 
 <Navbar />
-<main class="mx-auto mt-[4rem] max-w-2xl px-6 py-3">
+<main class="mx-auto mt-[4rem] min-h-screen max-w-2xl px-6 py-3">
 	{@render children()}
 </main>
 <Menu />
