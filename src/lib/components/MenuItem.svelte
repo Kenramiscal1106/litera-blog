035a6bac3960d174d5447ff6eb0b6614be04ed0d
Ken Ramiscal (Laptop) <code.ramiscalken@gmail.com>
2024-12-06 13:04:22 +0800

    add menu

---
+++ src/lib/components/MenuItem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet, SvelteComponent } from 'svelte';
+
+	const {
+		href,
+		// type,
+		active,
+		children
+	}: {
+		href: string;
+		// type: 'nav' | 'menu';
+		active: boolean;
+		children: Snippet;
+	} = $props();
+</script>
+
+<a {href} class={`block px-4 py-3 ${active ? 'bg-primary-500' : ''}`}>
+	<div class={`${active ? 'font-bold text-neutral-900' : ''} flex gap-3 text-xl`}>
+		{@render children()}
+	</div>
+</a>
