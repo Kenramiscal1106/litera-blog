1b512ee784174b3e5897aba7b96f91f01310e12c
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-12-06 20:08:32 +0800

    add rounded corners

--- src/lib/components/BlogDisplay.svelte
+++ src/lib/components/BlogDisplay.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 </script>
 
-<article class="bg-warm-100 my-6">
+<article class="my-6 overflow-hidden rounded-md bg-warm-100">
 	<!-- <img src="" alt="" /> -->
 	<div class="h-48 bg-gradient-to-tr from-primary-900 to-primary-600"></div>
 	<div class="flex flex-col gap-2 px-5 py-6">
 		<h1 class="text-3xl text-primary-800 sm:text-4xl">
 			<a href="/blog/demo"> Blog title </a>
 		</h1>
-		<div class="text-warm-600 flex text-base">
+		<div class="flex text-base text-warm-600">
 			<div>December 18, 2024 • 3 mins read</div>
 		</div>
-		<div class="text-warm-700 text-base leading-relaxed">
+		<div class="text-base leading-relaxed text-warm-700">
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magni odit perspiciatis
 			officiis debitis iusto quam ut accusantium enim et.
 		</div>
-		<div class="text-warm-700 flex items-center gap-2 text-base">
+		<div class="flex items-center gap-2 text-base text-warm-700">
 			<div class="aspect-square w-8 bg-gradient-to-tr from-primary-900 to-primary-600"></div>
 			<div>By Ken Ramiscal</div>
 		</div>
