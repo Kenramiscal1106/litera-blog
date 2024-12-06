0e4dcf427c83bc0dae7c39ebbd106ab4d977d5a3
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-12-06 21:09:30 +0800

    change styling

--- src/lib/components/MenuItem.svelte
+++ src/lib/components/MenuItem.svelte
@@ -12,7 +12,10 @@
 	} = $props();
 </script>
 
-<a {href} class={`block rounded-md px-4 py-3 ${$page.route.id === href ? 'bg-primary-500' : ''}`}>
+<a
+	{href}
+	class={`block rounded-md px-4 py-3 transition-colors duration-150 ${$page.route.id === href ? 'bg-primary-500' : ''}`}
+>
 	<div
 		class={`${$page.route.id === href ? 'fill-primary-900 font-bold text-warm-100' : ''} flex items-center gap-3 fill-primary-500 text-[1rem] text-lg`}
 	>
