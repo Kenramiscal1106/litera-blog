d7176a5b6fe2fbbef7de6ac69e5b86ec1c7e1c19
Ken Ramiscal (Laptop) <code.ramiscalken@gmail.com>
2024-12-10 11:50:04 +0800

    add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const { variant, children }: { variant: 'outline' | 'fill' | 'text'; children?: any } = $props();
+	const variantClasses = {
+		outline: 'border-neutral-200 text-primary-800',
+		fill: 'bg-primary-500 text-neutral-100',
+		text: 'text-primary-800'
+	};
+</script>
+
+<button class={`rounded-[.25rem] px-4 py-2 text-base sm:py-3 ${variantClasses[variant]}`}>
+	{@render children?.()}
+</button>
