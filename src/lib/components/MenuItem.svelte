6a433c59adef37ede0913c6f94e0563cf9606931
Ken Ramiscal (Laptop) <code.ramiscalken@gmail.com>
2024-12-10 11:51:28 +0800

    change functionality and add delay to navbar

--- src/lib/components/MenuItem.svelte
+++ src/lib/components/MenuItem.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import type { Snippet, SvelteComponent } from 'svelte';
+	import { fly } from 'svelte/transition';
+	const delayNav = 225;
 	const {
 		href,
-		// type,
+		type = 'link',
+		delay = 0,
+		onclick,
 		children
 	}: {
-		href: string;
-		// type: 'nav' | 'menu';
+		href?: string;
+		type?: 'link' | 'button';
+		delay?: number;
+		onclick?: () => void;
 		children: Snippet;
 	} = $props();
 </script>
 
 <a
-	{href}
-	class={`block rounded-md px-4 py-3 transition-colors duration-150 ${$page.route.id === href ? 'bg-primary-500' : ''}`}
+	href={type === 'link' ? href : $page.route.id}
+	class={`block rounded-[4px] border-x-4 border-transparent px-3 py-3 transition-colors duration-150 ${$page.route.id === href ? 'border-l-primary-900 bg-primary-400' : ''}`}
+	{onclick}
+	in:fly={{ delay: (delayNav * 1) / 4 + delay, duration: 175, x: -200 }}
 >
 	<div
 		class={`${$page.route.id === href ? 'fill-primary-900 font-bold text-warm-100' : ''} flex items-center gap-3 fill-primary-500 text-[1rem] text-lg`}
