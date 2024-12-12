cca9aff429c89cd93c47a6dd5995e2e8f5f106c7
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-12-12 16:36:08 +0800

    add logic for loader and scroll problem

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,35 @@
 <script lang="ts">
-	import { afterNavigate } from '$app/navigation';
-	import { nav } from '$lib/components/store.svelte';
+	import { afterNavigate, beforeNavigate, onNavigate } from '$app/navigation';
+	import { nav, modal } from '$lib/components/store.svelte';
 	import Menu from '$lib/components/Menu.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 	import FeedbackModal from '$lib/components/FeedbackModal.svelte';
+	import Loader from '$lib/components/Loader.svelte';
 	let { children } = $props();
+	let loading = $state();
 	afterNavigate(() => {
 		nav.closeMenu();
+		loading = false;
+	});
+	beforeNavigate(() => {
+		loading = true;
+	});
+	$effect(() => {
+		if (nav.menuOpen) {
+			console.log(window.scrollY);
+		}
+		if (nav.menuOpen || modal.open) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'visible';
+		}
 	});
 </script>
 
+{#if loading}
+	<Loader />
+{/if}
 <Navbar />
 <main class="mx-auto mt-[4rem] max-w-2xl px-6 py-3">
 	{@render children()}
