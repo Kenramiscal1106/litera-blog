df1075e48650644110c67c023a2a654f3caeca68
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-12-14 07:46:12 +0800

    add Navlinks

---
+++ src/lib/components/Navlinks.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import type { Snippet } from 'svelte';
+
+	const { href, children }: { href: string; children: Snippet } = $props();
+</script>
+
+<a
+	class={`duration-175 transition-colors ${
+		$page.route.id === href
+			? 'font-bold text-primary-900'
+			: 'text-primary-500 hover:text-primary-800'
+	}`}
+	{href}
+>
+	{@render children()}
+</a>
