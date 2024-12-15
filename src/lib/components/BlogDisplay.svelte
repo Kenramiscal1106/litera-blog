3fe93f1f935d7c4ead0dda13a0a20b4fd0785c54
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-12-15 16:44:35 +0800

    update styling and spacing for blog display

--- src/lib/components/BlogDisplay.svelte
+++ src/lib/components/BlogDisplay.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { Post } from '$lib';
 
 	const props: Post = $props();
 </script>
 
-<article class="my-6 overflow-hidden rounded-md bg-warm-100 shadow-lg">
+<article class="overflow-hidden rounded-md bg-warm-100 shadow-lg">
 	<a href="/blog/{props.slug}">
-		<div class="aspect-video w-auto bg-gradient-to-tr from-primary-900 to-primary-600"></div>
+		<img
+			class="aspect-video w-auto bg-gradient-to-tr from-primary-300 to-primary-600"
+			src="{$page.url.origin}/images/{props.slug}.png"
+			alt={props.title}
+		/>
 		<div class="flex flex-col gap-2 px-5 py-6">
-			<h1 class="text-3xl text-primary-800 sm:text-4xl">{props.title}</h1>
-			<div class="flex text-base text-warm-600">
+			<h1 class="text-2xl text-primary-800 md:text-3xl">{props.title}</h1>
+			<div class="text-base text-warm-600">
 				<div>{props.date} • 3 mins read</div>
 			</div>
 			<div class="flex items-center gap-2 text-base text-warm-700">
